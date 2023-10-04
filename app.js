516e7a56e58349157747447c36057b959542a13f
Muhammad usman <musman0325@gmail.com>
2023-10-04 22:24:57 +0500

    5 chapter complete

---
+++ app.js
@@ -0,0 +1,130 @@
+// Chapter 1 (Alerts)
+// 1. Alert these following (individually):
+// I. First Name
+// II. Last Name
+// III. Email
+// IV. Phone Number
+// V. Password
+//2. Correct this statement: alert"You're learning JavaScript!";
+//3. Code an alert statement displaying any message you like.
+
+//Answer Chapter 1
+alert("Muhammad");
+alert("Usman");
+alert("musman0325@gmail.com");
+alert("03430863553");
+alert("&ali123cxz*");
+alert("You're learning JavaScript!");
+alert("Web 3.O & Metaverse");
+
+
+//Chapter 2 (Variables for string)
+
+// 1. Declare any variable in the camelCase format.
+var camelCaseVariable = "muhammadUsmanAli";
+
+// 2. Declare a variable of your choice without defining it. Then, in a second statement, assign it a string of your choice.
+var variableName;
+variableName = "This is a string.";
+
+// 3. Declare the variable teamName and Alert your Team name.
+const teamName = "Pakistan XI";
+alert(teamName);
+
+// 4. This statement has already been coded. var bestMan = "Charlie"; Assign the variable a new string.
+bestMan = "babarAzam";
+
+//    Chapter 3 (Variables for numbers)
+
+//1. Declare a variable "caseQty"
+var caseQty;
+//2. Assign to the variable caseQty, which has already been declared, the value 144.
+caseQty = 144;
+
+//3. Rewrite this statement so the variable can be used in a math operation.
+// Original statement:
+var num = "9";
+
+// Rewritten statement:
+var num = parseInt("9");
+
+//4. In one statement declare a variable. In a second statement assign it the sum of 2 numbers.
+var total = 1 + 2;
+//5. What is the value of orderTotal?
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+//The value of orderTotal is 110
+//6. In the first statement declare a variable and assign it a number. In the second statement, change the value of the variable by adding it together with a number.
+var number = 1;
+number += 1;
+
+//Chapter 4 (Variable names Legal and Illegal)
+
+//1. Correct this statement.
+// Original statement:
+var product cost = 3.45;
+
+// Corrected statement:
+var productCost = 3.45;
+
+//2. Rewrite this using camelCase.
+// Original statement:
+var Nameofband;
+
+// Rewritten statement:
+var bandName;
+//3. In a single statement declare a legally-named variable and assign a number to it.
+var legallyNamedVariable = 325;
+
+//4. Declare a variable that is a combination of your first and last names. Use camelCase.
+var myFirstName = "Muhammad";
+var myLastName = "Usman";
+var myFullName = myFirstName + myLastName ;
+
+//5. List the legal and illegal variables.
+
+//Legal variables
+var myName;
+var _myName;
+var $myName;
+var my325Name;
+
+//Illegal variables
+var 1myName;
+var my name;
+var var;
+
+//Chapter 5 (Math Expression I)
+// 1. What is the name and symbol of the arithmetic operator that gives you the remainder when one number is divided by another?
+var remainderOperator = "%";
+
+// 2. What is the value of num?
+var num = 20 % 6; // The value of num is 2.
+
+// 3. In a single statement, declare the variable largeNum and assign it the result of 1,000 multiplied by 2,000.
+var largeNum = 1000 * 2000;
+
+// 4. Assign to a variable the value represented by one variable subtracted from the value represented by another variable
+var variable1 = 20;
+var variable2 = 5;
+var subtractionResult = variable1 - variable2;
+
+// 5. Assign to a variable the remainder when one number is divided by another. The variable hasn't been declared beforehand. Make up the variable name.
+var someNumber = 25;
+var anotherNumber = 8;
+var remainder = someNumber % anotherNumber;
+
+// 6. Code an alert that displays the result of a multiplication on 2 numbers.
+var num1 = 5;
+var num2 = 10;
+var multiplicationResult = num1 * num2;
+alert("The result of the multiplication is: " + multiplicationResult);
+
+//Chapter 5 (Math Expression I)
+
+
+
+
+
+
